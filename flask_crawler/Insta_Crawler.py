ba4c613b525f8cd43a382840bdfce214a2b971e9
Christoph <Christoph_s@posteo.de>
2018-10-11 17:42:42 +0200

    11.10.2018

--- flask_crawler/Insta_Crawler.py
+++ flask_crawler/Insta_Crawler.py
@@ -12,40 +12,36 @@ class Insta_Crawler():
         options.add_argument("--headless")
 
         driver = webdriver.Firefox(firefox_options=options)
-        driver.implicitly_wait(10)
-        wait = WebDriverWait(driver, 10)
-
+        driver.implicitly_wait(5)
         driver.get(url)
 
-        search_bar = driver.find_element_by_id('twotabsearchtextbox')
+        try:
+            search_bar = driver.find_element_by_id('twotabsearchtextbox')
+            search_bar.send_keys('blueray movies 2018')
+            search_bar.submit()
 
-        search_bar.send_keys('blueray movies 2018')
+            items = driver.find_elements_by_xpath("//li[contains(@id, 'result_')]")
 
-        search_bar.submit()
+            item_list = []
 
-        items = driver.find_elements_by_xpath("//li[contains(@id, 'result_')]")
+            for item in items:
+                title = item.find_element_by_xpath(
+                    ".//h2[contains(@class, 'a-size-medium') and contains(@class, 's-inline')"
+                    "and contains(@class, 's-access-title') and contains(@class, 'a-text-normal')]")
 
-        item_list = []
+                prices = item.find_elements_by_xpath(".//span[contains(@class, 'sx-price-whole')]")
+                prices_fractional = item.find_elements_by_xpath(".//sup[contains(@class, 'sx-price-fractional')]")
+                items_info = item.find_elements_by_xpath(".//h3[contains(@data-attribute, '4K')"
+                                                          "or contains(@data-attribute, 'Blu-ray')"
+                                                          "or contains(@data-attribute, 'DVD')]")
 
-        for item in items:
-            title = item.find_element_by_xpath(
-                ".//h2[contains(@class, 'a-size-medium') and contains(@class, 's-inline')"
-                "and contains(@class, 's-access-title') and contains(@class, 'a-text-normal')]")
+                for price, price_fractional, item_in in zip(prices, prices_fractional, items_info):
+                    item_list.append({
+                        'title': title.get_attribute('data-attribute'),
+                        'attribute':  item_in.get_attribute('data-attribute'),
+                        'price':  price.text + ',' + price_fractional.text + '$'})
 
-            prices = item.find_elements_by_xpath(".//span[contains(@class, 'sx-price-whole')]")
-            prices_fractional = item.find_elements_by_xpath(".//sup[contains(@class, 'sx-price-fractional')]")
-            items_info = item.find_elements_by_xpath(".//h3[contains(@data-attribute, '4K')"
-                                                      "or contains(@data-attribute, 'Blu-ray')"
-                                                      "or contains(@data-attribute, 'DVD')]")
+            return item_list
 
-            for price, price_fractional, item_in in zip(prices, prices_fractional, items_info):
-                item_list.append({
-                    'title': title.get_attribute('data-attribute'),
-                    'attribute':  item_in.get_attribute('data-attribute'),
-                    'price':  price.text + ',' + price_fractional.text + '$'})
-
-        driver.quit()
-
-        return item_list
-
-
+        finally:
+            driver.quit()
--- flask_crawler/run.py
+++ flask_crawler/run.py
@@ -1,14 +1,7 @@
 from flask import Flask, jsonify
 from flask_crawler.Insta_Crawler import Insta_Crawler
-import json
 
 app = Flask(__name__)
-
-test_json = {
-    'id': 1,
-    'name': u'test',
-    'preis': 55
-}
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -16,8 +9,4 @@ def hello():
     crawler = Insta_Crawler()
     items = crawler.crawl()
 
-    d = {'Movie': [item for item in items]}
-
-    json_string = json.dumps(d)
-
-    return jsonify(json_string)
+    return jsonify({'Movie': [item for item in items]})
