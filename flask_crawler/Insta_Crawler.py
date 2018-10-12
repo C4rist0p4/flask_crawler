b9e2b105183576f18091267182e108eacf64d227
Christoph <Christoph_s@posteo.de>
2018-10-12 17:28:26 +0200

    12.10.2018

--- flask_crawler/Insta_Crawler.py
+++ flask_crawler/Insta_Crawler.py
@@ -1,47 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 class Insta_Crawler():
 
     def crawl(self):
-        url = "https://www.amazon.com"
+        url = "https://www.instagram.com/explore/tags/sport/"
 
         options = Options()
         options.add_argument("--headless")
 
         driver = webdriver.Firefox(firefox_options=options)
         driver.implicitly_wait(5)
-        driver.get(url)
 
         try:
-            search_bar = driver.find_element_by_id('twotabsearchtextbox')
-            search_bar.send_keys('blueray movies 2018')
-            search_bar.submit()
 
-            items = driver.find_elements_by_xpath("//li[contains(@id, 'result_')]")
+            driver.get(url)
 
-            item_list = []
+            most_popular_posts = driver.find_elements_by_class_name('v1Nh3')
 
-            for item in items:
-                title = item.find_element_by_xpath(
-                    ".//h2[contains(@class, 'a-size-medium') and contains(@class, 's-inline')"
-                    "and contains(@class, 's-access-title') and contains(@class, 'a-text-normal')]")
+            users_names = []
 
-                prices = item.find_elements_by_xpath(".//span[contains(@class, 'sx-price-whole')]")
-                prices_fractional = item.find_elements_by_xpath(".//sup[contains(@class, 'sx-price-fractional')]")
-                items_info = item.find_elements_by_xpath(".//h3[contains(@data-attribute, '4K')"
-                                                          "or contains(@data-attribute, 'Blu-ray')"
-                                                          "or contains(@data-attribute, 'DVD')]")
+            cou = 0
+            for i in most_popular_posts:
 
-                for price, price_fractional, item_in in zip(prices, prices_fractional, items_info):
-                    item_list.append({
-                        'title': title.get_attribute('data-attribute'),
-                        'attribute':  item_in.get_attribute('data-attribute'),
-                        'price':  price.text + ',' + price_fractional.text + '$'})
+                driver.execute_script("arguments[0].scrollIntoView();", i)
 
-            return item_list
+                hover = ActionChains(driver).move_to_element(i)
+                hover.click().perform()
+
+                users_names.append(driver.find_element_by_class_name('FPmhX').text)
+
+                driver.find_element_by_class_name('ckWGn').click()
+
+                cou += 1
+                if cou == 3:
+                    cou = 0
+                    break
+
+            for user_name in users_names:
+
+                driver.get('https://www.instagram.com/' + user_name)
+
+                user_data = driver.find_elements_by_class_name('-nal3')
+
+                print('Name: ' + user_name)
+
+                for info in user_data:
+                    print(info.text)
+
+                cou += 1
+                if cou == 3:
+                    break
+
+
+            return users_names
 
         finally:
             driver.quit()
